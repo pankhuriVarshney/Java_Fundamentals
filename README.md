60651853805a5c9d8da57289a251da74d3c52d81
Patz17 <call.me.bi.your.name17@gmail.com>
2023-07-29 23:17:52 +0530

    Print all Unique Numbers between a given range 'unique numbers are such that none of the digits in the number are repeated'
    Add a method int unique(lower, upper), which prints all generated unique numbers and returns the frequency.
    
    The aim of this program is to find an efficient way of solving the problem which skips invalid numbers instead of checking each consecutive number.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java_Fundamentals
-This repositary contains the progress of my basic Java programming. 
+This repository is a collection of some of my Java Programs I created during my learning process. 
+It contains use of various programming structures and Java packages. It also includes implementation of data structures such as linked lists, stack and queue
---
+++ uniqueNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class uniqueNumber
+{
+    protected int unique(int lower, int upper)
+    {
+        int i=lower;
+        while(i<=upper)
+        {
+            int len=Integer.toString(i).length();
+            //add each digit as an element in integer array
+        }
+    }
+   public static void main(String args[])
+   {
+
+   }
+
+}
