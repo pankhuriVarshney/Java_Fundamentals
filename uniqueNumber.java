0aa1f5bc6042c44fe8207541399db0bf506a717a
Patz17 <call.me.bi.your.name17@gmail.com>
2023-07-30 08:53:24 +0530

    Complete function to generate unique numbers within the given range
    Iterate from the lower range to the upper range.
    Store each digit of the iterating number in an array
    Check if two digits of the number are the same, if so, it increases the value of the lower placeholder by one
    If the same digit is '9', increase the digit before '9' by one and change the '9' into a '0'
    Once all the numbers are different, break out of the loop and print the number
    Count frequency and return it

--- uniqueNumber.java
+++ uniqueNumber.java
@@ -1,18 +1,48 @@
 import java.util.*;
 class uniqueNumber
 {
-    protected int unique(int lower, int upper)
+    static int num[];
+    static int len;
+    protected static int unique(int lower, int upper)
     {
         int i=lower;
+        int f=0;
         while(i<=upper)
         {
-            int len=Integer.toString(i).length();
-            //add each digit as an element in integer array
+            len=Integer.toString(i).length();
+            num=new int[len];
+            int temp=i;
+            for(int a=len-1;a>=0;a--)
+            {
+                num[a]=temp%10;
+                temp/=10;
+            }
+            for(int t=1;t<=len;t++)
+            {for(int j=0;j<len-1;j++)
+            {
+                for(int k=j+1;k<len;k++)
+                {
+                    if(num[j]==num[k])
+                    {
+                        if(num[k]==9)
+                        { num[k-1]++;   num[k]=0;   break;}                    
+                        else
+                        {num[k]++;}
+                    }
+                }
+            }}
+            temp=0;
+            for(int j=0;j<len;j++)
+            temp=temp*10+num[j];
+            System.out.println(temp);
+            f++; 
         }
+        return f;
+    }
     }
    public static void main(String args[])
    {
-
+        
    }
 
 }
